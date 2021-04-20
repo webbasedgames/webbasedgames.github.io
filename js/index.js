151020b0c97ca04c3809bb0d36b6a85de58b003a
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-04-20 12:20:17 +0530

    Edit adventure description

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ let games = [
   new Game('Tic Tac Toe', 'The classic Tic Tac Toe game', '../tictactoe.html'),
   new Game(
     'Adventure classic',
-    'A text based adventure game to pass the time for a while',
+    `A text based adventure game to pass the time for a while. You wake up in a strange place with a jar of blue slime next to you. What will this andventure lead to? Let's find out together.`,
     '../adventure.html'
   ),
   new Game('Snake Xenzia', 'The classic snake in a box game', '../snake.html'),
